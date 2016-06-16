faed25505ce389f109cf1e40c597a5b127d89193
Epix <git@exz.me>
2016-06-16 16:34:42 +0800

    basic login test

--- test_laundry_backend.py
+++ test_laundry_backend.py
@@ -1,7 +1,11 @@
 # -*- encoding: utf-8 -*-
 # Author: Epix
+import json
 import os
 import unittest
+
+from flask import jsonify
+
 import laundry_backend
 from models import db
 
@@ -12,17 +16,22 @@ class FlaskTestCase(unittest.TestCase):
         laundry_backend.app.config['TESTING'] = True
         db.init_app(laundry_backend.app)
         with laundry_backend.app.app_context():
-            if not os.path.exists('test.sqlite'):
-                db.create_all()
-            db.drop_all()
+            if os.path.exists('test.sqlite'):
+                db.drop_all()
+            db.create_all()
         self.app = laundry_backend.app.test_client()
 
     def tearDown(self):
         pass
 
+    def login(self, username, password):
+        rv = self.app.post('/token', data=json.dumps({'username': username, 'password': password}),
+                           headers={'content-type': 'application/json'})
+        return rv
+
     def test_login(self):
-        rv = self.app.get('/revoke')
-        pass
+        rv = self.login('a', '1')
+        self.assertEqual(rv.status_code, 200)
 
 
 if __name__ == '__main__':
